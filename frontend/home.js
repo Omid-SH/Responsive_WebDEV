745fc302480942f471467177ca22ad2a3880a58d
Omid-SH <hdomid@yahoo.com>
2020-11-21 21:51:26 +0330

    homepage commit

---
+++ frontend/home.js
@@ -0,0 +1,22 @@
+/* Items of big screen */
+var homeBigDataButton = document.getElementById("home_big_data_button");
+var homeBigHomeButton = document.getElementById("home_big_home_button");
+var homeBigGallery = document.getElementById("home_big_gallery");
+
+var homeBigGallery = document.getElementById("home_big_gallery");
+var homeBigGallery = document.getElementById("home_big_gallery");
+var homeBigGallery = document.getElementById("home_big_gallery");
+ 
+
+homeBigDataButton.addEventListener("click", setGallery(1))
+
+/* In this part we will set all buttons listeners and animations */
+/*
+function setGallery(p) {
+    if(p==0) {
+        
+        return;
+    }
+
+}
+*/
